c94d21898921a2a7d19c397c13c515e0c02ce3ba
KJafro <Kurtj222@outlook.com>
2023-02-11 15:05:47 +0000

    ok

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("https://everydaybeing.onrender.com/api/auth/register", {
+      const res = await axios.post("https://everydaybeing.onrender.com/auth/register", {
         username, 
         email, 
         password
