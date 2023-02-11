756fb42a2a9305d14b6ca2180580672ab95f7303
KJafro <Kurtj222@outlook.com>
2023-02-11 23:36:33 +0000

    ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
             <span className="headerTitleSm">React & Node</span>
             <span className="headerTitleLg">Blog</span>
         </div>
-        <img className ="headerImg" src="https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+        <img className ="headerImg" src="./avatar.jpg" alt="" />
     </div>
   )
 }
