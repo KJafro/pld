396e9a9fc72ec48988aaf49c7dc46c7a7d5e3567
KJafro <Kurtj222@outlook.com>
2023-02-11 18:49:47 +0000

    ok

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -18,7 +18,7 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
         <div className="sidebarItem">
-          <div className="sidebarTitle">ABOUT ME</div>
+          <div className="sidebarTitle">ABOUT ME!</div>
           <img src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia corporis vel beatae dignissimos a in provident 
           repellat alias amet odit modi voluptate similique tempora ea voluptatibus.</p>
