a52c97e670fe81042fd0e86907dd54b070293de9
KJafro <Kurtj222@outlook.com>
2023-02-11 19:16:21 +0000

    ok

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("https://everydaybeing.onrender.com//posts" + search)
+      const res = await axios.get("https://everydaybeing.onrender.com/posts" + search)
       setPosts(res.data)
     }
     fetchPosts()
