3fe6214f41937d1f98e025204b47cd8c635d21de
KJafro <Kurtj222@outlook.com>
2023-02-13 22:46:35 +0000

    ok

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -20,7 +20,7 @@ export default function Post({post}) {
             <span className="postTitle">{post.title}</span>
             </Link>
         <hr />
-        <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+        <span className="postDate">{new Date(post.createdAt).toDateString().format( 'MMMM Do YYYY, h:mm:ss a')}</span>
         </div>
         <p className="postDesc">{post.desc}</p>
     </div>
