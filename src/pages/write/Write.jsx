0fa7ca510373a60a12720f59dc0818dec0a8199c
KJafro <Kurtj222@outlook.com>
2023-02-15 18:01:00 +0000

    ok

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -33,7 +33,7 @@ export default function Write() {
     }
     try {
       const res = await axiosInstance.post("/posts", newPost);
-      window.location.replace("https://everydaybeingfront.onrender.com/#/post/" + res.data._id);
+      window.location.replace("https://everydaybeingfront.onrender.com/#/about/")
     } catch (err) {
 
     }
