1bd044a0db64b5e31156a454763d5779bde72e4c
KJafro <Kurtj222@outlook.com>
2023-02-19 20:14:45 +0000

    ok

--- src/pages/success/ContactSuccess.jsx
+++ src/pages/success/ContactSuccess.jsx
@@ -1,14 +1,13 @@
 import "./success.css"
 import { Link } from "react-router-dom"
 import { Oval } from "react-loader-spinner"
-import 'animate.css';
 
 export default function ContactSuccess() {
   return (
     
     <div className="container">
         <div className="successtxt">
-            <h2 class="animate__animated animate__fadeInDown">EMAIL RECEIVED!</h2>
+            <h2>EMAIL RECEIVED!</h2>
             <Oval
             height={70}
             width={70}
