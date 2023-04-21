f47b35179f9a0c756870a4ccd26991c4640061ee
KJafro <Kurtj222@outlook.com>
2023-04-21 22:16:12 +0100

    Dissertation updating SW

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,34 +1,38 @@
-const CACHE_NAME = "version-1"
-const urlsToCache = ["index.html", "offline.html"]
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+const self = this;
 
-this.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log("Open Cache")
-            return cache.addAll(urlsToCache);
-        })
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
     )
-})
-
-this.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then ((res) => {
-            return fetch(event.request).catch(() => caches.match('offline.html'))
-        })
-    )
-})
-
-this.addEventListener('activate', (event) => {
-    const cacheWhiteList = []
-    cacheWhiteList.push(CACHE_NAME)
-    event.waitUntil(caches.keys().then((cacheNames) => Promise.all(
-        cacheNames.map((cacheName) => {
-            if(!cacheWhiteList.includes(cacheName)){
-                return caches.delete(cacheName)
-            }
-        })
-    )))
-})
+});
 
 console.log("Service Worker Successfully Loaded!");
 
