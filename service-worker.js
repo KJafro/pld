cd77c9dce2d6309a4174b9870127ac594c6653cd
KJafro <Kurtj222@outlook.com>
2023-04-23 21:22:02 +0100

    Updating fetch subscription

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('push', function(event) {
+  console.log('Received a push notification', event);
+
+  if (event.data) {
+    showNotification(event.data.text());
+  } else {
+    console.log('Push notification has no payload.');
+  }
+});
+
+function showNotification(message) {
+  self.registration.showNotification('My App', {
+    body: message,
+  });
+}
--- src/pages/subscribe/Subscribe.jsx
+++ src/pages/subscribe/Subscribe.jsx
@@ -4,86 +4,64 @@ import TopBar from '../../components/topbar/TopBar'
 
 export default function Subscribe() {
 
-//   const publicVapidKey =
-//   "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
+  const publicVapidKey =
+  "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
 
-// if ("serviceWorker" in navigator) {
-//   send().catch(err => console.error(err));
-// }
-
-// async function send() {
-//   console.log("Registering Worker!");
-//   const register = await navigator.serviceWorker.register("/worker.js", {
-//     scope: "/"
-//   });
-//   console.log("Service Worker Registered!");
-
-//   console.log("Registering Push!");
-//   const subscription = await register.pushManager.subscribe({
-//     userVisibleOnly: true,
-//     applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-//   });
-//   console.log("Push Registered!");
-
-//   console.log("Sending Push!!!");
-//   await fetch("https://everydaybeing.onrender.com/subscribe", {
-//     method: "POST",
-//     body: JSON.stringify(subscription),
-//     headers: {
-//       "content-type": "application/json"
-//     }
-//   });
-//   console.log("Push Sent!");
-// }
-
-// function urlBase64ToUint8Array(base64String) {
-//   const padding = "=".repeat((4 - base64String.length % 4) % 4);
-//   const base64 = (base64String + padding)
-//     .replace(/\-/g, "+")
-//     .replace(/_/g, "/");
-
-//   const rawData = window.atob(base64);
-//   const outputArray = new Uint8Array(rawData.length);
-
-//   for (let i = 0; i < rawData.length; ++i) {
-//     outputArray[i] = rawData.charCodeAt(i);
-//   }
-//   return outputArray;
-// }
-
-
-//   const subScribe = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axiosInstance.post("/subscribe", {
-//       });
-// console.log('res')
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
-
-async function regi () {
-  let sw = await navigator.serviceWorker.register('./sw.js')
-  console.log(sw)
+if ("serviceWorker" in navigator) {
+  send().catch(err => console.error(err));
 }
 
-// addEventListener('load', async () => {
-//   let sw = await navigator.serviceWorker.register('./sw.js')
-//   console.log(sw)
-// })
+async function send() {
+  console.log("Registering Worker!");
+  const register = await navigator.serviceWorker.register("/worker.js", {
+    scope: "/"
+  });
+  console.log("Service Worker Registered!");
 
+  console.log("Registering Push!");
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+  });
+  console.log("Push Registered!");
 
-async function subscribe() {
-  let sw = await navigator.serviceWorker.ready;
-  let push = await sw.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: 'BLQGq3C889qOEdVcZnGaur0qq3yO5U3fycxvVpKjqTKIj2dhb4nPSpOcVJmcPKnAfovGs0mKi0Igb0Vu5GIdJcs'
-  })
-  console.log(JSON.stringify(push))
+  console.log("Sending Push!!!");
+  await fetch("https://everydaybeing.onrender.com/subsscribe", {
+    method: "POST",
+    body: JSON.stringify(subscription),
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+  console.log("Push Sent!");
 }
 
-subscribe()
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+
+  const subScribe = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axiosInstance.post("/subscribe", {
+      });
+console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
 
 
@@ -91,10 +69,8 @@ subscribe()
     <>
     <TopBar/>
     <div>Subscribe</div>
-    <form className="registerForm">
+    <form className="registerForm" onSubmit={subScribe}>
     <button className="registerButton" type="submit">Sub</button>
-    <button className="registerButton" type="submit" onClick={regi()}>Register</button>
-    <div id='result'>ss</div>
     </form>
     </>
   )
