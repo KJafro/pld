eead3382526a0f786f1eb44e658618989c03bdbe
KJafro <Kurtj222@outlook.com>
2023-04-25 18:09:28 +0100

    ok

--- public/worker.js
+++ public/worker.js
@@ -2,7 +2,7 @@ console.log("Service Worker Successfully Loaded!YE");
 
 self.addEventListener("push", e => {
   const data = e.data.json();
-  console.log("Push Recieved! PublicFolder");
+  console.log("Push Recieved! PublicFolderssss");
   self.registration.showNotification(data.title, {
     body: "Click here to visit us again!",
     icon: "https://th.bing.com/th/id/R.f2ff5a5ce46d7cd7621fdf09ee2d4529?rik=Rgzj8LY5sDh%2b8A&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2015%2f07%2flogo-with-transparent-background-gallery_100204.png&ehk=9cRdKRl2FytHnQBYgmag%2fw40lKoDavuJZHu59y%2b6U24%3d&risl=&pid=ImgRaw&r=0"
