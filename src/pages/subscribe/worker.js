946dcb6e20b2862606a26001bccbe8ee7b7d94a5
KJafro <Kurtj222@outlook.com>
2023-04-25 17:41:56 +0100

    ok

--- src/pages/subscribe/worker.js
+++ src/pages/subscribe/worker.js
@@ -2,7 +2,7 @@ console.log("Service Worker Successfully LoadedYE");
 
 self.addEventListener("push", e => {
   const data = e.data.json();
-  console.log("Push Recieved!!!!!!YE");
+  console.log("Push Recieved! SubFolder");
   self.registration.showNotification(data.title, {
     body: "EDB",
     icon: "https://th.bing.com/th/id/R.f2ff5a5ce46d7cd7621fdf09ee2d4529?rik=Rgzj8LY5sDh%2b8A&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2015%2f07%2flogo-with-transparent-background-gallery_100204.png&ehk=9cRdKRl2FytHnQBYgmag%2fw40lKoDavuJZHu59y%2b6U24%3d&risl=&pid=ImgRaw&r=0"
