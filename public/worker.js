47875203458802bf65d2220409c25e35ce8ba9a6
KJafro <Kurtj222@outlook.com>
2023-04-25 18:31:12 +0100

    test

--- public/worker.js
+++ public/worker.js
@@ -1,10 +1,10 @@
-// console.log("Service Worker Successfully Loaded!YE");
+console.log("Service Worker Successfully Loaded!YE");
 
-// self.addEventListener("push", e => {
-//   const data = e.data.json();
-//   console.log("Push Recieved! PublicFolderrrrr");
-//   self.registration.showNotification(data.title, {
-//     body: "Click here to visit us again!",
-//     icon: "https://th.bing.com/th/id/R.f2ff5a5ce46d7cd7621fdf09ee2d4529?rik=Rgzj8LY5sDh%2b8A&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2015%2f07%2flogo-with-transparent-background-gallery_100204.png&ehk=9cRdKRl2FytHnQBYgmag%2fw40lKoDavuJZHu59y%2b6U24%3d&risl=&pid=ImgRaw&r=0"
-//   });
-// });
+self.addEventListener("push", e => {
+  const data = e.data.json();
+  console.log("Push Recieved! PublicFolderrrrr");
+  self.registration.showNotification(data.title, {
+    body: "Click here to visit us again!",
+    icon: "https://th.bing.com/th/id/R.f2ff5a5ce46d7cd7621fdf09ee2d4529?rik=Rgzj8LY5sDh%2b8A&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2015%2f07%2flogo-with-transparent-background-gallery_100204.png&ehk=9cRdKRl2FytHnQBYgmag%2fw40lKoDavuJZHu59y%2b6U24%3d&risl=&pid=ImgRaw&r=0"
+  });
+});
