132ca843ce545af8bfc3bf70d345d59561f57fac
KJafro <Kurtj222@outlook.com>
2023-05-01 19:39:33 +0100

    updating pn image

--- public/worker.js
+++ public/worker.js
@@ -2,9 +2,9 @@ console.log("Service Worker Successfully Loaded!YE");
 
 self.addEventListener("push", e => {
   const data = e.data.json();
-  console.log("Push Recieved! PublicFolderrrrr");
+  console.log("Push Recieved!!!");
   self.registration.showNotification(data.title, {
     body: "Click here to visit us again!",
-    icon: "https://th.bing.com/th/id/R.f2ff5a5ce46d7cd7621fdf09ee2d4529?rik=Rgzj8LY5sDh%2b8A&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2015%2f07%2flogo-with-transparent-background-gallery_100204.png&ehk=9cRdKRl2FytHnQBYgmag%2fw40lKoDavuJZHu59y%2b6U24%3d&risl=&pid=ImgRaw&r=0"
+    icon: "https://images.unsplash.com/photo-1580250642511-1660fe42ad58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1325&q=80"
   });
 });
