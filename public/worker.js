6a8ba90dd0b010468e7f83a8718590a969075946
KJafro <Kurtj222@outlook.com>
2023-05-02 17:18:38 +0100

    ok

--- public/worker.js
+++ public/worker.js
@@ -1,10 +1,24 @@
 // console.log("Service Worker Successfully Loaded!YE");
 
-self.addEventListener("push", e => {
-  const data = e.data.json();
-  console.log("Push Recieved!!! it has");
-  self.registration.showNotification(data.title, {
-    body: "Click here to visit us again!",
-    icon: "https://images.unsplash.com/photo-1580250642511-1660fe42ad58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1325&q=80"
-  });
+self.addEventListener('push', function (e) {
+  var options = {
+      body: 'Visit now!',
+      icon: 'images/volcano.jpg',
+      vibrate: [100, 50, 100],
+      data: {
+          dateOfArrival: Date.now(),
+          primaryKey: '2'
+      },
+      actions: [
+          {action: 'explore', title: 'Explore!',
+              icon: 'images/checkmark.png'},
+              {action: 'close', title: 'Close',
+              icon: 'images/xmark.png'},
+      ]
+  };
+  e.waitUntil(
+  self.registration.showNotification('We have added a new blog/podcast!', options)
+  );
 });
+
+self.addEventListener('fetch',() => console.log("fetch"));
