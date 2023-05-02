eb753df99d7e6072501f58a9cb8a1b1cf4db43e1
KJafro <Kurtj222@outlook.com>
2023-05-02 16:41:07 +0100

    ok

--- public/worker.js
+++ public/worker.js
@@ -2,7 +2,7 @@
 
 self.addEventListener("push", e => {
   const data = e.data.json();
-  console.log("Push Recieved!!!");
+  console.log("Push Recieved!!! it has");
   self.registration.showNotification(data.title, {
     body: "Click here to visit us again!",
     icon: "https://images.unsplash.com/photo-1580250642511-1660fe42ad58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1325&q=80"
