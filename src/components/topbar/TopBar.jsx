ec29d8e11bb96e4db1ed2ffd9feeaa0673b3a8f5
KJafro <Kurtj222@outlook.com>
2023-07-03 22:58:15 +0100

    Pics

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -25,6 +25,7 @@ export default function TopBar() {
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
   };
+  
   return (
     <div className="top">
       <div className="topLeft">
