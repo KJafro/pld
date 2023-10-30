c468fad51ea7434d5a642f801325ea496001b1dd
KJafro <Kurtj222@outlook.com>
2023-10-30 11:04:29 +0000

    Update

--- src/components/podcast/Podcast.jsx
+++ src/components/podcast/Podcast.jsx
@@ -1,35 +1,30 @@
-import "./podcast.css"
-import {Link} from "react-router-dom"
+import "./podcast.css";
+import { Link } from "react-router-dom";
 import { Fade } from "react-awesome-reveal";
-import { SpotifyEmbed } from 'spotify-embed';
-import LazyLoad from 'react-lazy-load';
-import TopBarPodcast from "../../components/topbar/TopBarPodcast"
+import { SpotifyEmbed } from "spotify-embed";
+import LazyLoad from "react-lazy-load";
+import TopBarPodcast from "../../components/topbar/TopBarPodcast";
 
-export default function Podcast({podcast}) {
-  let moment = require ('moment');
+export default function Podcast({ podcast }) {
+  let moment = require("moment");
   const cleanedTitle = podcast.title.replace(/%/g, "").replace(/\s+/g, "-");
 
   return (
-<div className="podcastFront">
+    <div className="podcastFront">
       {podcast.photo && (
-        // <img class="postImg" src={PF + post.photo/} alt="" /> 
-        <Link to={`/podcast/${cleanedTitle}`} className="links"><SpotifyEmbed class="podcastImg" src={podcast.photo} /></Link>
+        <Link to={`/podcast/${cleanedTitle}`} className="links">
+          <SpotifyEmbed class="podcastImg" src={podcast.photo} />
+        </Link>
       )}
-        <div className="podcastInfo">
-            {/* <div className="postCats">{
-              post.categories.map(c=> (
-                <span className="postCat">ssdasdsa{c.name}</span>
-              ))
-            }
-            // </div> */}
-            <Link to={`/podcast/${cleanedTitle}`} className="link">
-            <p className="podcastTitle">{podcast.title}
-            </p>
-            <p className="podcastDate">{moment(podcast.createdAt).format('Do MMMM YYYY | h:mm a')}</p>
-            <p className="podcastDesc">{podcast.desc}</p>
-            </Link>
-            {/* <p className="podcastUser">{podcast.username}</p> */}
-        </div>
-        </div>
-  )
+      <div className="podcastInfo">
+        <Link to={`/podcast/${cleanedTitle}`} className="link">
+          <p className="podcastTitle">{podcast.title}</p>
+          <p className="podcastDate">
+            {moment(podcast.createdAt).format("Do MMMM YYYY | h:mm a")}
+          </p>
+          <p className="podcastDesc">{podcast.desc}</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/pages/podcast/Podcast.jsx
+++ src/pages/podcast/Podcast.jsx
@@ -1,44 +1,46 @@
-import { useEffect, useState } from "react"
-import Header from "../../components/header/Header"
-import Podcasts from "../../components/podcasts/Podcasts"
-import { Pagination } from "../../components/pagination/Pagination"
-import "./podcast.css"
-import { axiosInstance } from "../../config"
-import { useLocation } from "react-router"
-import TopBarPodcast from "../../components/topbar/TopBarPodcast"
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Podcasts from "../../components/podcasts/Podcasts";
+import { Pagination } from "../../components/pagination/Pagination";
+import "./podcast.css";
+import { axiosInstance } from "../../config";
+import { useLocation } from "react-router";
+import TopBarPodcast from "../../components/topbar/TopBarPodcast";
 
 export default function Podcast() {
   useEffect(() => {
-    document.title = "Everyday Being | Podcasts"
+    document.title = "Everyday Being | Podcasts";
   }, []);
   const [podcasts, setPodcasts] = useState([]);
-  const {search} = useLocation();
+  const { search } = useLocation();
   const [currentPage, setCurrentPage] = useState(1);
   const [podcastsPerPage, setPodcastsPerPage] = useState(8);
 
   useEffect(() => {
     const fetchPodcasts = async () => {
-      const res = await axiosInstance.get("/podcasts" + search)
-      setPodcasts(res.data.reverse())
-    }
-    fetchPodcasts()
-  },[search])
+      const res = await axiosInstance.get("/podcasts" + search);
+      setPodcasts(res.data.reverse());
+    };
+    fetchPodcasts();
+  }, [search]);
 
   const indexOfLastPost = currentPage * podcastsPerPage;
   const indexOfFirstPost = indexOfLastPost - podcastsPerPage;
   const currentPodcasts = podcasts.slice(indexOfFirstPost, indexOfLastPost);
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <>
-    <TopBarPodcast/>
+      <TopBarPodcast />
       <Header />
       <div className="home">
-        <Podcasts podcasts={currentPodcasts}/>
+        <Podcasts podcasts={currentPodcasts} />
       </div>
-      <Pagination podcastsPerPage={podcastsPerPage}
-         totalPodcasts={podcasts.length}
-          paginate={paginate}/>
+      <Pagination
+        podcastsPerPage={podcastsPerPage}
+        totalPodcasts={podcasts.length}
+        paginate={paginate}
+      />
     </>
   );
 }
